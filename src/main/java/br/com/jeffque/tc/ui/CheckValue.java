ff3843cbffbdfece3cf1c70a29f60f9f3c3f7ecf
Jefferson Quesado <jeff.quesado@gmail.com>
2016-06-28 21:54:47 -0300

    Um Check com Value =D

---
+++ src/main/java/br/com/jeffque/tc/ui/CheckValue.java
@@ -0,0 +1,19 @@
+package br.com.jeffque.tc.ui;
+
+import br.com.jeffque.tc.util.Valueable;
+import totalcross.ui.Check;
+
+public class CheckValue<V> extends Check implements Valueable<V> {
+	private V value;
+
+	public CheckValue(V value, String text) {
+		super(text);
+		this.value = value;
+	}
+	
+	@Override
+	public V getValue() {
+		return value;
+	}
+
+}
