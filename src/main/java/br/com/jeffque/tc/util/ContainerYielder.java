9a0970b2118960e2b2bca850a5b996c0ae36efa7
Jefferson Quesado <jeff.quesado@gmail.com>
2016-11-28 19:09:16 -0300

    Um gerador de container?

---
+++ src/main/java/br/com/jeffque/tc/util/ContainerYielder.java
@@ -0,0 +1,8 @@
+package br.com.jeffque.tc.util;
+
+import br.com.jeffque.tc.ui.BaseContainer;
+import br.com.jeffque.tc.ui.BaseTCMain;
+
+public abstract class ContainerYielder {
+	public abstract BaseContainer yield(BaseTCMain<?> base);
+}
