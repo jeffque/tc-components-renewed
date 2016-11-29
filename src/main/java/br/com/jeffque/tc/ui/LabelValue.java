565b8fc4d3a42f2ef821a88ce7bec204ee98a7a4
Jefferson Quesado <jeff.quesado@gmail.com>
2016-11-29 16:44:09 -0300

    Label valorada

---
+++ src/main/java/br/com/jeffque/tc/ui/LabelValue.java
@@ -0,0 +1,36 @@
+package br.com.jeffque.tc.ui;
+
+import br.com.jeffque.magic.util.Valueable;
+import br.com.jeffque.tc.util.Presenter;
+import totalcross.ui.Label;
+
+public class LabelValue<V> extends Label implements Valueable<V> {
+	Presenter<V> presenter;
+	V value;
+
+	public LabelValue(Presenter<V> presenter, V value) {
+		super(presenter.stringfy(value));
+		this.presenter = presenter;
+		this.value = value;
+	}
+
+	public LabelValue(Presenter<V> presenter, V value, int align) {
+		super(presenter.stringfy(value), align);
+		
+		this.presenter = presenter;
+		this.value = value;
+	}
+
+	public LabelValue(Presenter<V> presenter, V value, int align, int fore, boolean bold) {
+		super(presenter.stringfy(value), align, fore, bold);
+		
+		this.presenter = presenter;
+		this.value = value;
+	}
+
+	@Override
+	public V getValue() {
+		return value;
+	}
+
+}
