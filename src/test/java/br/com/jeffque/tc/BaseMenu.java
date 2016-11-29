5b934ff38cebe507ffff21c73d1d5780aa1060b3
Jefferson Quesado <jeff.quesado@gmail.com>
2016-11-28 23:54:35 -0300

    Me baseando no container yielder para ser feliz

--- src/test/java/br/com/jeffque/tc/BaseMenu.java
+++ src/test/java/br/com/jeffque/tc/BaseMenu.java
@@ -7,40 +7,48 @@ import br.com.jeffque.tc.multislider.MultiSliderTest;
 import br.com.jeffque.tc.ui.BaseContainer;
 import br.com.jeffque.tc.ui.BaseTCMain;
 import br.com.jeffque.tc.ui.FirstContainer;
+import br.com.jeffque.tc.util.ContainerYielder;
 import totalcross.ui.Button;
 import totalcross.ui.event.ControlEvent;
 import totalcross.ui.event.PressListener;
 
 public class BaseMenu extends FirstContainer {
-	public List<BaseContainer> containers;
-	public void povoaContainers(BaseTCMain<?> baseTest) {
-		containers = new ArrayList<>();
-		containers.add(new MultiSliderTest(baseTest));
+	public List<ContainerYielder> yielders;
+	BaseTCMain<?> base;
+	public void povoaContainers() {
+		yielders = new ArrayList<>();
+		yielders.add(new ContainerYielder() {
+			@Override
+			public BaseContainer yield(BaseTCMain<?> base) {
+				return new MultiSliderTest(base);
+			}
+		});
 	}
 
 	public BaseMenu(BaseTCMain<?> baseTest) {
 		super(baseTest);
-		povoaContainers(baseTest);
+		base = baseTest;
+		povoaContainers();
 	}
 	
 	@Override
 	public void initUI() {
 		super.initUI();
 		
-		for (BaseContainer containerTest: containers) {
-			addButton(containerTest);
+		for (ContainerYielder yielderContainerTest: yielders) {
+			addButton(yielderContainerTest);
 		}
 	}
 
-	private void addButton(BaseContainer container) {
+	private void addButton(ContainerYielder yielderContainer) {
 		PressListener abrirContainer = new PressListener() {
 			
 			@Override
 			public void controlPressed(ControlEvent arg0) {
-				present(container);
+				present(yielderContainer.yield(base));
 			}
 		};
-		Button btn = new Button(container.getClass().getName().replaceAll("[^.]*\\.", ""));
+		Button btn = new Button(yielderContainer.yield(base).getClass().getName().replaceAll("[^.]*\\.", ""));
 		btn.addPressListener(abrirContainer);
 		add(btn, LEFT, AFTER, FILL, PREFERRED);
 	}
