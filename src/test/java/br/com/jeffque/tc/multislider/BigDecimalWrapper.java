da7ecffe0128e2292b1d81df1de2bc1df180cb16
Jefferson Quesado <jeff.quesado@gmail.com>
2016-11-29 03:40:39 -0300

    Um wrapper de bigdecimal para começar o teste com big decimal

---
+++ src/test/java/br/com/jeffque/tc/multislider/BigDecimalWrapper.java
@@ -0,0 +1,15 @@
+package br.com.jeffque.tc.multislider;
+
+import totalcross.util.BigDecimal;
+
+public class BigDecimalWrapper {
+	BigDecimal value;
+
+	public BigDecimal getValue() {
+		return value;
+	}
+
+	public void setValue(BigDecimal value) {
+		this.value = value;
+	}
+}
