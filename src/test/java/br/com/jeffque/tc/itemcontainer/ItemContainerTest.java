a10819bdd3959d5a8a66cb53dc33717e85067684
Jefferson Quesado <jeff.quesado@gmail.com>
2017-01-10 09:40:21 -0300

    Pondo uma tela para testar o ItemContainer

---
+++ src/test/java/br/com/jeffque/tc/itemcontainer/ItemContainerTest.java
@@ -0,0 +1,36 @@
+package br.com.jeffque.tc.itemcontainer;
+
+import br.com.jeffque.tc.ui.BaseContainer;
+import br.com.jeffque.tc.ui.BaseTCMain;
+import br.com.jeffque.tc.ui.ItemContainer;
+import totalcross.ui.Button;
+import totalcross.ui.event.ControlEvent;
+import totalcross.ui.event.PressListener;
+
+public class ItemContainerTest extends BaseContainer {
+	ItemContainer<Restaurante> itemContainer = new ItemContainer<>(new RestauranteViewHandler());
+	RestauranteBuilderView restauranteBuilderView = new RestauranteBuilderView();
+	
+	public ItemContainerTest(BaseTCMain<?> baseTest) {
+		super(baseTest);
+	}
+	
+	@Override
+	public void initUI() {
+		super.initUI();
+		
+		Button addBtn = new Button("Adicionar Restaurante");
+		
+		addBtn.addPressListener(new PressListener() {
+			
+			@Override
+			public void controlPressed(ControlEvent arg0) {
+				itemContainer.addObject(restauranteBuilderView.getValue());
+			}
+		});
+		
+		add(restauranteBuilderView, LEFT, AFTER, FILL, PREFERRED);
+		add(addBtn, LEFT, AFTER, FILL, PREFERRED);
+		add(itemContainer, LEFT, AFTER, FILL, FILL);
+	}
+}
